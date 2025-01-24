56241f6b7cb54cdca06338866d4059094471dc6d
Kostya_B <beztalannyjkonstantin@gmail.com>
2025-01-24 13:01:16 +0200

    24.011 first commit

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask,g
-import sqlite3 as sq 
+import sqlite3 as sq
+from flask import Flask, render_template,g,request, flash,redirect
 
 DATABASE="test.db"
 DEBUG=True
-SECRET_KEY="1234qwer"
+SECRET_KEY="1234qwert"
 
 app=Flask(__name__)
 app.config.from_object(__name__)
@@ -20,16 +20,30 @@ def create_db():
     db.close()
 
 def get_db():
-    if not hasattr(g,"link-db"):
-        return connect_db()   
+    if not hasattr(g,"link_db"):
+        g.link_db=connect_db()
+    return g.link_db
 
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g,"link_db"):
-        return connect_db().close() 
-    
+        g.link_db.close()
 
-@app.route("/")
+@app.route("/add_user", methods=["POST","GET"])
+def add():
+    db=get_db()
+    if request.method=="POST":
+        if request.form.get("name") and request.form.get("email"):
+            name=request.form.get("name")
+            email=request.form.get("email")
+            db.cursor().execute("INSERT INTO users VALUES(Null,?,?)", (name,email))
+            db.commit()  
+            return redirect("/users")
+    return render_template("add.html")
+
+@app.route("/users")
 def index():
-    return "Hello"
-
+    db=get_db()
+    name=db.cursor().execute("SELECT name FROM users")
+    result=name.fetchall()
+    return render_template("index.html",names=result)
