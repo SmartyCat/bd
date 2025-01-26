5788f9250dd974eca999c2ec1cd4207259fdbfe1
Kostya_B <beztalannyjkonstantin@gmail.com>
2025-01-26 12:21:19 +0200

    26.01.2025

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
-from flask import Flask,g,render_template,flash,request
+from flask import Flask, g,redirect,render_template, request,flash
 import sqlite3 as sq
 
-DATABASE="movies.db"
-DEBUG=True
-SECRET_KEY="1234QWER"
+DATABASE="users.db"
+SECRET_KEY="1234qwer"
 
 app=Flask(__name__)
 app.config.from_object(__name__)
 
 def connect_db():
-    conn=sq.connect("movies.db")
+    conn=sq.connect(app.config["DATABASE"])
     return conn
 
 def create_db():
     db=connect_db()
-    with app.open_resource("my_sql.sql",mode="r") as f:
+    with app.open_resource("my_sql.sql", mode="r") as f:
         db.cursor().executescript(f.read())
     db.commit()
     db.close()
@@ -25,27 +24,28 @@ def get_db():
     return g.link_db
 
 @app.teardown_appcontext
-def close_db(error):
+def close(error):
     if hasattr(g,"link_db"):
         g.link_db.close()
 
-@app.route("/")
-def main():
-    db=get_db()
-    movies=db.cursor().execute("SELECT title,year FROM movies")
-    movies=movies.fetchall()
-    return render_template("main.html",movies=movies)
 
 @app.route("/add",methods=["POST","GET"])
-def add():
+def main():
     db=get_db()
     if request.method=="POST":
-        title=request.form.get("title")
-        year=request.form.get("year")
-        if title and year:
-            db.cursor().execute("INSERT INTO movies VALUES(Null,?,?)",(title,year))
+        name=request.form.get("name")
+        age=request.form.get("age")
+        if name and age:
+            db.cursor().execute("INSERT INTO users VALUES(Null,?,?)", (name,age))
             db.commit()
-            flash("Данные добавлены",category="success")
+            flash("Данные успешно отправлены")
         else:
-            flash("Вознилка ошибка. Попробуйте еще.",category="wrong")
+            flash("Ошибка")
     return render_template("add.html")
+
+@app.route("/")
+def add():
+    db=get_db()
+    users=db.cursor().execute("SELECT name, age FROM users")
+    users=users.fetchall()
+    return render_template("main.html",users=users)
