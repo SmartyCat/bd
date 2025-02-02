34503403c02d16e6e95560e2656dc4f81256face
Kostya_B <beztalannyjkonstantin@gmail.com>
2025-02-02 15:16:27 +0200

    second commit 02.02

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
 from flask import Flask, g, render_template, flash, request, redirect, session
 import sqlite3 as sq
 from datetime import datetime
-from werkzeug.security import generate_password_hash,check_password_hash
-from flask_login import LoginManager,login_user,login_required,current_user,logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import (
+    LoginManager,
+    login_user,
+    login_required,
+    current_user,
+    logout_user,
+)
 from UserLogin import UserLogin
+from fdatabase import FDataBase
 
 
 DATABASE = "tasks.db"
 SECRET_KEY = "1234qwer"
-userlogin=None
+userlogin = None
 app = Flask(__name__)
 app.config.from_object(__name__)
-login_manager=LoginManager(app)
-login_manager.login_view="login"
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+
 
 @login_manager.user_loader
 def load_user(user_id):
-    db=get_db()
-    return UserLogin().fromDB(user_id,db)
+    db = get_db()
+    return UserLogin().fromDB(user_id, db)
+
 
 def connect_db():
     conn = sq.connect(app.config["DATABASE"])
@@ -47,33 +56,23 @@ def close_db(error):
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    db = get_db()
+    db=get_db()
     if not current_user.is_authenticated:
         return render_template("unlogin.html")
-    
-    tasks = (
-        db.cursor()
-        .execute(
-            "SELECT id,description, CASE WHEN is_done='0' THEN 'НЕ ВЫПОЛНЕНО' ELSE 'Выполнено' END AS result, time,deadline FROM tasks WHERE user_id=? ORDER BY time",(current_user.get_id(),)
-        )
-        .fetchall()
-    )
+
+    tasks = FDataBase(db).getTask(current_user.get_id())
     return render_template("index.html", tasks=tasks)
 
 
 @app.route("/add", methods=["POST", "GET"])
 @login_required
 def add():
-    db = get_db()
+    db=get_db()
     if request.method == "POST":
         description = request.form.get("description")
-        deadline=request.form.get("deadline")
+        deadline = request.form.get("deadline")
         if description and deadline:
-            db.cursor().execute(
-                "INSERT INTO tasks VALUES(Null,?,?,?,?,?)",
-                (description, False, datetime.now().strftime("%Y-%m-%d"),deadline,current_user.get_id()),
-            )
-            db.commit()
+            FDataBase(db).addTask(description,deadline,current_user.get_id())
             flash("Данные успешно отправлены")
         else:
             flash("Данные введены некорректно")
@@ -82,17 +81,15 @@ def add():
 
 @app.route("/delete/<int:task_id>", methods=["POST", "GET"])
 def delete(task_id):
-    db = get_db()
-    db.cursor().execute(f"DELETE FROM tasks WHERE id={task_id}")
-    db.commit()
+    db=get_db()
+    FDataBase(db).deleteTask(task_id)
     return redirect("/")
 
 
 @app.route("/complete/<int:task_id>")
 def complete(task_id):
     db = get_db()
-    db.cursor().execute(f"UPDATE tasks SET is_done='Выполнено' WHERE id=?",(task_id,))
-    db.commit()
+    FDataBase(db).completeTask(task_id)
     return redirect("/")
 
 
@@ -100,64 +97,57 @@ def complete(task_id):
 def edit(task_id):
     db = get_db()
     form_data = request.form.to_dict()
-    form_data["desc"] = (
-        db.cursor()
-        .execute(f"SELECT description FROM tasks WHERE id=?",(task_id,))
-        .fetchone()
-    )
+    form_data["desc"] = FDataBase(db).getDescription(task_id)
     if request.method == "POST":
         result = request.form.get("desc")
-        new_deadline=request.form.get("new_deadline")
+        new_deadline = request.form.get("new_deadline")
         if result and new_deadline:
-            print(result)
-            db.cursor().execute(
-                "UPDATE tasks SET description=?,deadline=? WHERE id=?", (result,new_deadline,task_id)
-            )
-            db.commit()
+            FDataBase(db).editTask(result,new_deadline,task_id)
             return redirect("/")
         else:
             flash("ошибка")
     return render_template("edit.html", form_data=form_data, task_id=task_id)
 
 
-@app.route("/registr",methods=["POST","GET"])
+@app.route("/registr", methods=["POST", "GET"])
 def registr():
     if current_user.is_authenticated:
         return redirect("/")
-    db=get_db()
-    if request.method=="POST":
-        email=request.form.get("email")
-        password=request.form.get("password")
-        password2=request.form.get("password2")
-        if email and password==password2:
-            pas=generate_password_hash(password)
-            db.cursor().execute("INSERT INTO users VALUES(Null,?,?)",(email,pas))
-            db.commit()
+    db = get_db()
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        password2 = request.form.get("password2")
+        if email and password == password2:
+            pas = generate_password_hash(password)
+            FDataBase(db).addUser(email,pas)
             return redirect("login")
     return render_template("registr.html")
 
-@app.route("/login",methods=["POST","GET"])
+
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    if  current_user.is_authenticated:
+    if current_user.is_authenticated:
         return redirect("/")
-    db=get_db()
-    if request.method=="POST":
-        user=db.cursor().execute("SELECT * FROM users WHERE email= ? LIMIT 1",(request.form.get("email_login"),)).fetchone()
-        if user and check_password_hash(user[2],request.form.get("password_login")):
-            userlogin=UserLogin().create(user)
-            rm=True if request.form.get("remember") else False
-            login_user(userlogin,remember=rm)
-            return redirect ("/")
+    db = get_db()
+    if request.method == "POST":
+        user = FDataBase(db).getUser(request.form.get("email_login"))
+        if user and check_password_hash(user[2], request.form.get("password_login")):
+            userlogin = UserLogin().create(user)
+            rm = True if request.form.get("remember") else False
+            login_user(userlogin, remember=rm)
+            return redirect("/")
         else:
             return flash("Ошибка")
     return render_template("login.html")
+
 
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
-    return redirect ("/")
-    
+    return redirect("/")
+
 
 @app.errorhandler(404)
 def page_not_found(error):
