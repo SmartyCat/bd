428e63da76551a679e26aff31e5fa77e3d1571c9
Kostya_B <beztalannyjkonstantin@gmail.com>
2025-02-04 18:48:56 +0200

    add gitignore

--- check_time.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-def is_valide_date(date_string,date_format="%Y-%m-%d-%H-%M"):
-        if datetime.strptime(date_string,date_format):
-            return True
-        else:
-              return False
